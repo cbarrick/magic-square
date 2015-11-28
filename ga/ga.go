de0f0942a9d105af8263af890f64c3d797cb71c6
Chris Barrick <cbarrick1@gmail.com>
2015-11-28 14:51:34 -0500

    Refactor GA into library

--- ga/ga.go
+++ ga/ga.go
@@ -1,43 +1,43 @@
-package main
+package ga
 
 import (
-	"fmt"
 	"math/rand"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/cbarrick/evo"
 	"github.com/cbarrick/evo/perm"
-	"github.com/cbarrick/evo/pop/graph"
+	"github.com/cbarrick/evo/pop/gen"
 	"github.com/cbarrick/evo/sel"
 )
 
-// Tuneables
-const (
-	dim = 4 // dimension of the problem
-)
+// An instance represents a problem instance being solved. This type carries the
+// global data used for the optimization. The schema is the partially filled
+// square that we're trying to complete, where values greater than or equal to
+// 0 appearing in the schema must appear in all genes at the same position, and
+// values less than 0 in the schema represent variable values in the genes.
+type instance struct {
+	sync.Mutex
+	order  int   // order of the square being solved
+	schema []int // schema of the square being solved, -1 for variables
+	count  int   // number of fitness evaluations
+}
 
-// Global objects
-var (
-	// Counts the number of fitness evaluations
-	count struct {
-		sync.Mutex
-		n int
-	}
-)
-
-// The magic type specifies our genome. We use a permutation of [0,dim^2) to
+// The Magic type specifies our genome. We use a permutation of [0,order^2) to
 // represent the square. Note that Wikipedia specifies magic squares as
-// permutations of (0,dim^2]. This difference may have subtle implications on
+// permutations of (0,order^2]. This difference may have subtle implications on
 // the theory.
-type magic struct {
+type Magic struct {
 	gene []int // the square, interpreted as the concatination of the rows
+	inst *instance
 }
 
 // String returns a representation of the square. The output is a valid Prolog
 // term, and the numeric values are incremented by 1 to match the theory on
 // Wikipedia. Thus the output can be verified directly by the Prolog solver.
-func (m magic) String() (s string) {
+func (m Magic) String() (s string) {
+	order := m.inst.order
 	n := 0
 	s += "["
 	for i := range m.gene {
@@ -48,7 +48,7 @@ func (m magic) String() (s string) {
 			s += "["
 		}
 		s += strconv.Itoa(m.gene[i] + 1)
-		n = (n + 1) % dim
+		n = (n + 1) % order
 		if n == 0 {
 			s += "]"
 		}
@@ -58,20 +58,21 @@ func (m magic) String() (s string) {
 }
 
 // Close does nothing because we use no closable resources.
-func (m *magic) Close() {}
+func (m *Magic) Close() {}
 
 // Fitness returns the number of rows, columns, and diagonal that do not contain
 // conflicts. The fitness is only computed once across all calls to the method
 // by using a sync.Once.
-func (m *magic) Fitness() (fitness float64) {
-	sum := dim * (dim*dim - 1) / 2
+func (m *Magic) Fitness() (fitness float64) {
+	order := m.inst.order
+	sum := order * (order*order - 1) / 2
 	var part int // partial sum, used to compute sums of rows, cols, diags.
 
 	// rows
-	for i := 0; i < dim; i++ {
+	for i := 0; i < order; i++ {
 		part = 0
-		for j := 0; j < dim; j++ {
-			part += m.gene[dim*i+j]
+		for j := 0; j < order; j++ {
+			part += m.gene[order*i+j]
 		}
 		if part == sum {
 			fitness++
@@ -79,10 +80,10 @@ func (m *magic) Fitness() (fitness float64) {
 	}
 
 	// columns
-	for i := 0; i < dim; i++ {
+	for i := 0; i < order; i++ {
 		part = 0
-		for j := 0; j < dim; j++ {
-			part += m.gene[dim*j+i]
+		for j := 0; j < order; j++ {
+			part += m.gene[order*j+i]
 		}
 		if part == sum {
 			fitness++
@@ -91,48 +92,65 @@ func (m *magic) Fitness() (fitness float64) {
 
 	// diagonals
 	part = 0
-	for i := 0; i < dim; i++ {
-		part += m.gene[dim*i+i]
+	for i := 0; i < order; i++ {
+		part += m.gene[order*i+i]
 	}
 	if part == sum {
 		fitness++
 	}
 	part = 0
-	for i := 0; i < dim; i++ {
-		part += m.gene[dim*(i+1)-i-1]
+	for i := 0; i < order; i++ {
+		part += m.gene[order*(i+1)-i-1]
 	}
 	if part == sum {
 		fitness++
 	}
 
 	// increment the counter of fitness evals
-	count.Lock()
-	count.n++
-	count.Unlock()
+	m.inst.Lock()
+	m.inst.count++
+	m.inst.Unlock()
 
 	return fitness
 }
 
 // Evolve specifies the inner loop of our evolutionary algorithm.
-func (m *magic) Evolve(suitors ...evo.Genome) evo.Genome {
+func (m *Magic) Evolve(suitors ...evo.Genome) evo.Genome {
+	order := m.inst.order
+	size := order * order
+
 	// Creation:
 	// We allocate a new genome for the child on the heap
-	child := &magic{gene: make([]int, dim*dim)}
+	child := &Magic{gene: make([]int, order*order), inst: m.inst}
 
 	// Crossover:
 	// We're using a diffusion model. Thus we pick one suitor to cross with the
 	// current genome in this spot. We pick the mate using a single random
 	// binary tournament. We use cycle crossover to inherit positioning.
-	mate := sel.BinaryTournament(suitors...).(*magic)
+	mate := sel.BinaryTournament(suitors...).(*Magic)
 	perm.CycleX(child.gene, m.gene, mate.gene)
 
 	// Mutation:
 	// 20% chance of a single random swap, 4% chance of two swaps, 0.8% chance
 	// of three swaps, etc.
 	for {
-		n := rand.Float64()
-		if n < 0.2 {
-			perm.RandSwap(child.gene)
+		chance := rand.Float64()
+		if chance < 0.2 {
+			var i, j int
+			p := perm.New(size)
+			for i = 0; i < size; i++ {
+				if m.inst.schema[p[i]] < 0 {
+					break
+				}
+			}
+			for j = i + 1; j < size; j++ {
+				if m.inst.schema[p[j]] < 0 {
+					break
+				}
+			}
+			if i != size && j != size {
+				child.gene[p[i]], child.gene[p[j]] = child.gene[p[j]], child.gene[p[i]]
+			}
 		} else {
 			break
 		}
@@ -146,61 +164,114 @@ func (m *magic) Evolve(suitors ...evo.Genome) evo.Genome {
 	return child
 }
 
-func main() {
-	fmt.Println("Dimension:", dim)
-
-	// Setup:
-	// We create an initial random population to evolve in a diffusion model.
-	init := make([]evo.Genome, 1024)
-	for i := range init {
-		init[i] = &magic{gene: rand.Perm(dim * dim)}
+// sqrt returns the integer part of the square root of n
+func sqrt(n int) int {
+	n64 := int64(n)
+	res := int64(0)
+	one := int64(1) << 62
+	for n64 < one {
+		one >>= 2
 	}
-	pop := graph.Hypercube(init)
+	for one != 0 {
+		if res+one <= n64 {
+			n64 -= res + one
+			res += one << 1
+		}
+		res >>= 1
+		one >>= 2
+	}
+	return int(res)
+}
+
+// seed creates a random set of n Magic genomes to be used as an initial
+// population for the given problem instance
+func seed(inst *instance, n int) []evo.Genome {
+	init := make([]evo.Genome, n)
+	size := len(inst.schema)
+	for i := range init {
+		init[i] = &Magic{
+			gene: perm.New(size),
+			inst: inst,
+		}
+		for j := range inst.schema {
+			if inst.schema[j] < 0 {
+				continue
+			}
+			gene := init[i].(*Magic).gene
+			k := perm.Search(gene, inst.schema[j])
+			gene[j], gene[k] = gene[k], gene[j]
+		}
+	}
+	return init
+}
+
+// Solve returns a Magic representing the solution to a magic square schema. The
+// schema is a partially filled magic square, interpreted as the concatination
+// of the rows of the square. Values greater than or equal to 0 appearing in the
+// schema must appear in the solution at the same position, and values less than
+// 0 in the schema represent unknown values.
+//
+// The population is reseeded occasionally if the solution is not found. The
+// return value rsc is the "reseed count" giving the number of times reseeding
+// occurs.
+func Solve(schema []int) (soln *Magic, rsc int) {
+	var (
+		// the interface into the GA loop
+		pop evo.Population
+
+		// the number of values in the square
+		size = len(schema)
+
+		// the order of the problem
+		order = sqrt(size)
+
+		// the size of the population
+		popz = 10 * order
+
+		// the problem instance bein solved
+		inst = &instance{
+			order:  order,
+			schema: schema,
+		}
+
+		// variables used to reseed the population
+		reseedT = 4 * time.Second
+		reseedC = time.After(reseedT)
+		reseed  = func() {
+			rsc++
+			pop.Close()
+			s := seed(inst, popz)
+			s[popz-1] = evo.Max(pop)
+			pop = gen.New(s)
+			pop.Start()
+			reseedC = time.After(reseedT)
+		}
+	)
+
+	// start the GA in the background
+	pop = gen.New(seed(inst, popz))
 	pop.Start()
 
-	// Tear-down:
-	// Cleanup and print a solution if we have one.
-	defer func() {
-		pop.Close()
-		max := evo.Max(pop)
-		fmt.Println()
-		if max.Fitness() != dim*2+2 {
-			fmt.Println("Solution Not Found")
-			fmt.Println("Best:", max)
-		} else {
-			fmt.Println("Solution:", max)
-		}
-	}()
-
-	// Run:
-	// Collect and print stats and evaluate the termination conditions.
 	for {
-		count.Lock()
-		n := count.n
-		count.Unlock()
-		stats := pop.Stats()
+		select {
 
-		// "\x1b[2K" is the escape code to clear the line
-		fmt.Printf("\x1b[2K\rCount: %7d | Max: %2.0f | Min: %2.0f | Mean: %3.1f | SD: %6.6g",
-			n,
-			stats.Max(),
-			stats.Min(),
-			stats.Mean(),
-			stats.StdDeviation())
+		// reseed the population periodically with exponential backoff
+		case <-reseedC:
+			reseed()
+			reseedT *= 2
 
-		// We've found the solution when max is dim*2+2
-		if stats.Max() == dim*2+2 {
-			return
-		}
-
-		// We've converged once the deviation is less than 0.01
-		if stats.StdDeviation() < 1e-2 {
-			return
-		}
-
-		// Force stop after 2,000,000 fitness evaluations
-		if n > 2e6 {
-			return
+		// stop when we find a solution
+		// or reseed if we converge prematurely
+		default:
+			s := pop.Stats()
+			if s.Max() == float64(order*2+2) {
+				pop.Close()
+				soln = evo.Max(pop).(*Magic)
+				return soln, rsc
+			}
+			if s.RSD() < 0.00001 {
+				reseed()
+			}
 		}
 	}
 }
